324fbd7a90edacb8ea963b0ca49f032f9449fa70
mounirhassouni <hassouni.mounir@gmail.com>
2023-05-08 15:44:05 +0100

    home apdate2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/NavBar.css'
+import logo from '../assets/logop.png'
 
 function NavBar() {
   return (
     <nav className='navbar'>
-      <img></img>
+      <img src={logo}></img>
       <div  className='flex'>
         <Link to="/" className='flex-child'>Home</Link>
         <Link to="/menu" className='flex-child'>Menu</Link>
