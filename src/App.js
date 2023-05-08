a0dcfc59da9c2abc1b11b48f06ccb94ff435523b
mounirhassouni <hassouni.mounir@gmail.com>
2023-05-08 15:04:28 +0100

    home apdate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import './styles/App.css';
+import React, { Component } from 'react';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import NavBar from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello There!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/menu" element={<Menu/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/contact" element={<Contact/>} />
+        </Routes>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,18 @@
- 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/NavBar.css'
+
+function NavBar() {
+  return (
+    <nav className='navbar'>
+      <div  className='flex'>
+        <div><Link to="/">Home</Link></div>
+        <div><Link to="/menu">Menu</Link></div>
+        <div><Link to="/about">About</Link></div>
+        <div><Link to="/contact">Contact</Link></div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
